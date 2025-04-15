fd89975c86464c428db9a8796f907b79065c65ad
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-15 23:14:15 +0200

    Create base structure for Nav component

---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]'>
+        <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Nav;
