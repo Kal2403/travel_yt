bce12b2ae7a49cba428163694d88b2b11a08ad8d
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-17 23:55:15 +0200

    Enable client-side rendering

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,14 +1,11 @@
+"use client"
 import React from 'react';
-import Nav from './Nav';
-import MobileNav from './MobileNav';
 
 const ResponsiveNav = () => {
-  return (
-    <div>
-        <Nav />
-        <MobileNav />
-    </div>
-  )
+    return (
+        <div>
+        </div>
+    )
 }
 
 export default ResponsiveNav;
