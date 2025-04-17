95581e21814391a5c5a71369a96e810be9b56e39
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-17 23:56:25 +0200

    Create ResponsiveNav component with mobile nav state

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -4,8 +4,13 @@ import Nav from './Nav';
 import MobileNav from './MobileNav';
 
 const ResponsiveNav = () => {
+    const [showNav, setShowNav] = useState(false);
+    const handleShowNav = () => setShowNav(true);
+    const handleCloseNav = () => setShowNav(false);
     return (
         <div>
+            <Nav openNav={handleShowNav} />
+            <MobileNav showNav={showNav} closeNav={handleCloseNav} />
         </div>
     )
 }
