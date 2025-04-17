4f07b0de4a0b49b064ba288102a41baec617bb94
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-17 23:43:10 +0200

    Enable client-side rendering

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,24 +1,25 @@
+"use client";
 import { navLinks } from '@/constant/constant';
 import Link from 'next/link';
 import React from 'react';
 import { TbAirBalloon } from 'react-icons/tb';
 
 const Nav = () => {
-  return (
-    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]'>
-        <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
-            <div className='flex items-center space-x-2'>
-                <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col'>
-                    <TbAirBalloon className='w-6 h-6 text-white'/>
+    return (
+        <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] fixed w-full'>
+            <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
+                <div className='flex items-center space-x-2'>
+                    <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col'>
+                        <TbAirBalloon className='w-6 h-6 text-white' />
+                    </div>
+                    <h1 className='text-xl md:text-2xl text-white uppercase font-bold'>Tripi</h1>
                 </div>
-                <h1 className='text-xl md:text-2xl text-white uppercase font-bold'>Tripi</h1>
-            </div>
-            <div className='hidden lg:flex items-center space-x-10'>
-                {
-                    navLinks.map((link) => {
-                        return (
-                            <Link href={link.url} key={link.id}>
-                                <p className='
+                <div className='hidden lg:flex items-center space-x-10'>
+                    {
+                        navLinks.map((link) => {
+                            return (
+                                <Link href={link.url} key={link.id}>
+                                    <p className='
                                     relative
                                     text-white
                                     text-base
@@ -37,15 +38,15 @@ const Nav = () => {
                                     after:duration-300
                                     after:origin-right'
                                     >{link.label}
-                                </p>
-                            </Link>
-                        )
-                    })
-                }
+                                    </p>
+                                </Link>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Nav;
