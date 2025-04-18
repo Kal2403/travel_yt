165a568993d49ce5d9c86994457f2e501f316a18
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-18 23:22:42 +0200

    Add dark overlay on top of video background

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 const Hero = () => {
     return (
         <div className='relative w-full h-[120vh] sm:h-[100vh]'>
+            <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70'></div>
+            
         </div>
     )
 }
