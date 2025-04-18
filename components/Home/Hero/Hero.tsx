e7e346da65e42d398bff4ca4f26374b0eff9dcd6
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-18 23:23:17 +0200

    Add fullscreen looping video background

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,10 +1,12 @@
+import SearchBox from '@/components/Helper/SearchBox';
+import Link from 'next/link';
 import React from 'react';
 
 const Hero = () => {
     return (
         <div className='relative w-full h-[120vh] sm:h-[100vh]'>
             <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70'></div>
-            
+            <video src='/images/hero1.mp4' autoPlay muted loop preload='metadata' className='w-full h-full object-cover'/>
         </div>
     )
 }
