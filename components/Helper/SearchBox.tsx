2cedc89e1cb0d7fdc179b91f471072a14fe6cf73
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-18 23:28:14 +0200

    Create SearchBox component with base layout

---
+++ components/Helper/SearchBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () => {
+  return (
+    <div className='bg-white rounded-lg p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center justify-center gap-8 mt-4 sm:mt-12 w-[95%] sm:w-[80%]'>
+
+    </div>
+  )
+}
+
+export default SearchBox;
