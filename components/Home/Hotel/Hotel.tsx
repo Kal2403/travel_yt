d5961ecee3f809987e53def2fc477de53d2d81bf
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-20 00:22:41 +0200

    Create base Hotel component with section heading and Add to Home.tsx

---
+++ components/Home/Hotel/Hotel.tsx
@@ -0,0 +1,12 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
+import React from 'react';
+
+const Hotel = () => {
+  return (
+    <div className='pt-20 pb-20'>
+        <SectionHeading heading='Recommended Hotels'/>
+    </div>
+  )
+}
+
+export default Hotel;
