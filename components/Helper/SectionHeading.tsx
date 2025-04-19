575f1a26d1e23859eac1606cbc55a8955e286dd5
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-19 16:24:20 +0200

    Create SectionHeading with props and Add SectionHeading to Destination

---
+++ components/Helper/SectionHeading.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+    heading: string;
+};
+
+const SectionHeading = ({heading}: Props) => {
+  return (
+    <div className='w-[80%] mx-auto'>
+    </div>
+  )
+}
+
+export default SectionHeading;
--- components/Home/Destination/Destination.tsx
+++ components/Home/Destination/Destination.tsx
@@ -1,8 +1,10 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react';
 
 const Destination = () => {
   return (
     <div className='pt-20 pb-20'>
+        <SectionHeading heading='Exploring Popular Destination' />
     </div>
   )
 }
