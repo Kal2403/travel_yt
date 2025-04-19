89628e6b96246fb82685606e64c4615daf80e3d6
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-20 00:27:13 +0200

    Add hotel image with overlay and heart icon

--- components/Home/Hotel/HotelCard.tsx
+++ components/Home/Hotel/HotelCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaHeart } from 'react-icons/fa';
 
 type Props = {
     hotel: {
@@ -15,6 +16,13 @@ type Props = {
 const HotelCard = ({hotel}:Props) => {
   return (
     <div>
+        <div className='relative h-[300px] w-full rounded-lg cursor-pointer group overflow-hidden'>
+            <div className='absolute top-4 right-4 z-20 w-8 h-8 bg-white rounded-full text-black flex items-center justify-center flex-col'>
+                <FaHeart className='w-3 h-3' />
+            </div>
+            <div className='absolute inset-0 bg-black opacity-20 z-10'></div>
+            <img src={hotel.image} alt={hotel.name} className='overflow-hidden h-full w-full transition-all decoration-300 object-cover group-hover:scale-110' />
+        </div>
     </div>
   )
 }
