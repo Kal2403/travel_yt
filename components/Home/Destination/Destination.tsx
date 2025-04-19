9436816f1ce919033ae71f3502615931e9be31de
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-19 16:19:45 +0200

    Create base Destination component and Add to Home

---
+++ components/Home/Destination/Destination.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Destination = () => {
+  return (
+    <div className='pt-20 pb-20'>
+    </div>
+  )
+}
+
+export default Destination;
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -3,12 +3,12 @@ import Hero from './Hero/Hero';
 import Destination from './Destination/Destination';
 
 const Home = () => {
-  return (
-    <div className='overflow-hidden h-[10000px]'>
-      <Hero />
-      <Destination />
-    </div>
-  )
+    return (
+        <div className='overflow-hidden h-[10000px]'>
+            <Hero />
+            <Destination />
+        </div>
+    )
 }
 
 export default Home;
