c9e7e34e2d3b4d8494cb6c4bfd968e6e429f62c1
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-20 00:27:56 +0200

    Add hotel name and location text

--- components/Home/Hotel/HotelCard.tsx
+++ components/Home/Hotel/HotelCard.tsx
@@ -23,6 +23,10 @@ const HotelCard = ({hotel}:Props) => {
             <div className='absolute inset-0 bg-black opacity-20 z-10'></div>
             <img src={hotel.image} alt={hotel.name} className='overflow-hidden h-full w-full transition-all decoration-300 object-cover group-hover:scale-110' />
         </div>
+        <div>
+            <h1 className='mt-4 text-lg font-semibold text-blue-950 hover:text-black cursor-pointer transition-all duration-300'>{hotel.name}</h1>
+            <p className='text-sm text-gray-600 mt-3 font-medium mb-6'>{hotel.location}</p>
+        </div>
     </div>
   )
 }
