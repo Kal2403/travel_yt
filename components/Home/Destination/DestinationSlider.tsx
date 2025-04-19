6ad414a8a610cd900fd678575db18d5214741529
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-19 16:29:04 +0200

    Setup DestinationSlider component and import dependencies
    feat: create DestinationSlider component and setup Carousel

---
+++ components/Home/Destination/DestinationSlider.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
+
+const DestinationSlider = () => {
+    return (
+        <Carousel>
+
+        </Carousel>
+
+    )
+}
+
+export default DestinationSlider;
