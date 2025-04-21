45e5f1f8c8daf7e50e383b0034632f636ce7e656
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-21 23:42:09 +0200

    Create base WhyChooseCard component with props

---
+++ components/Home/WhyChoose/WhyChooseCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+    image: string;
+    title: string;
+}
+
+const WhyChooseCard = ({image, title}: Props) => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default WhyChooseCard;
