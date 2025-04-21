f61f1171646d21984d3827d5d6d672b392d51d61
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-21 23:48:18 +0200

    Create base Review section with background and layout and Add to Home

---
+++ components/Home/Reviews/Review.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Review = () => {
+  return (
+    <div className='pt-20 pb-20 flex items-center justify-center flex-col bg-[#13357b]'>
+        <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-10'>
+        </div>
+    </div>
+  )
+}
+
+export default Review;
