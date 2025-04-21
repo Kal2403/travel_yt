72042c481f82518193898042f18bd8471167489a
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-21 23:38:21 +0200

    Create base WhyChoose component with section heading and Add to Home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Hero from './Hero/Hero';
 import Destination from './Destination/Destination';
 import Hotel from './Hotel/Hotel';
+import WhyChoose from './WhyChoose/WhyChoose';
 
 const Home = () => {
     return (
-        <div className='overflow-hidden h-[10000px]'>
+        <div className='overflow-hidden'>
             <Hero />
             <Destination />
             <Hotel />
+            <WhyChoose />
         </div>
     )
 }
---
+++ components/Home/WhyChoose/WhyChoose.tsx
@@ -0,0 +1,12 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
+import React from 'react';
+
+const WhyChoose = () => {
+  return (
+    <div className='pt-16- pb-24'>
+        <SectionHeading heading='Why Choose Us' />
+    </div>
+  )
+}
+
+export default WhyChoose;
