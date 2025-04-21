73664c6cc2704dbeec51ba92b4485a2c2227e0c9
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-21 23:55:24 +0200

    Add review text and star icons

--- components/Home/Reviews/ReviewSlider.tsx
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -7,6 +7,7 @@ import 'swiper/css/effect-cards';
 
 import {EffectCards} from 'swiper/modules';
 import { reviewData } from '@/data/data';
+import { FaStar } from 'react-icons/fa';
 
 const ReviewSlider = () => {
   return (
@@ -15,6 +16,16 @@ const ReviewSlider = () => {
             {
                 reviewData.map((data) => (
                     <SwiperSlide key={data.id} className='bg-white rounded-3xl block'>
+                        <div className='w-[80%] mx-auto mt-16'>
+                            <p className='text-xs sm:text-sm md:text-base font-semibold'>{data.review}</p>
+                            <div className='flex items-center mt-4'>
+                                <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                                <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                                <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                                <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                                <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                            </div>
+                        </div>
                     </SwiperSlide>
                 ))
             }
