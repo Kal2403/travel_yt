a14e7caa1cc0643b416ef5e794d07799bff72d07
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-21 23:50:14 +0200

    Add overall rating and star icons

--- components/Home/Reviews/Review.tsx
+++ components/Home/Reviews/Review.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
 
 const Review = () => {
   return (
@@ -7,6 +8,19 @@ const Review = () => {
             <div>
                 <h1 className='text-2xl font-semibold text-white'>What our customers are saying us?</h1>
                 <p className='mt-6 text-gray-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a unde distinctio deleniti cum dolore tempora magni. Facere ut totam minus incidunt culpa</p>
+                <div className='mt-6 flex items-center space-x-6'>
+                    <div>
+                        <p className='text-2xl font-bold text-white'>4.88</p>
+                        <p className='text-white mb-2'>Overall Rating</p>
+                        <div className='flex items-center'>
+                            <FaStar className='text-white' />
+                            <FaStar className='text-white' />
+                            <FaStar className='text-white' />
+                            <FaStar className='text-white' />
+                            <FaStar className='text-white' />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
