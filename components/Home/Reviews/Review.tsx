0be7d1c138410dec4ce3faffe4a0615b3c25c8e7
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-21 23:49:33 +0200

    Add customer satisfaction text and description

--- components/Home/Reviews/Review.tsx
+++ components/Home/Reviews/Review.tsx
@@ -4,6 +4,10 @@ const Review = () => {
   return (
     <div className='pt-20 pb-20 flex items-center justify-center flex-col bg-[#13357b]'>
         <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-10'>
+            <div>
+                <h1 className='text-2xl font-semibold text-white'>What our customers are saying us?</h1>
+                <p className='mt-6 text-gray-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a unde distinctio deleniti cum dolore tempora magni. Facere ut totam minus incidunt culpa</p>
+            </div>
         </div>
     </div>
   )
