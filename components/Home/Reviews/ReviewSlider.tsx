fd2adfcb2dbf2cdf52d8551582b46bc72430995f
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-21 23:54:54 +0200

    Map and render reviewData inside SwiperSlide

---
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from 'react';
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+import {EffectCards} from 'swiper/modules';
+import { reviewData } from '@/data/data';
+
+const ReviewSlider = () => {
+  return (
+    <div>
+        <Swiper effect={"cards"} grabCursor={true} modules={[EffectCards]} className='md:w-[450px] md:h-[350px] w-[90%] h-[300px]'>
+            {
+                reviewData.map((data) => (
+                    <SwiperSlide key={data.id} className='bg-white rounded-3xl block'>
+                    </SwiperSlide>
+                ))
+            }
+        </Swiper>
+    </div>
+  )
+}
+
+export default ReviewSlider
