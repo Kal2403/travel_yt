85a1721ce9a910b41bccfd5d2cb4e558529ca996
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-21 23:42:49 +0200

    add image, title, and static description to WhyChooseCard

--- components/Home/WhyChoose/WhyChooseCard.tsx
+++ components/Home/WhyChoose/WhyChooseCard.tsx
@@ -8,6 +8,9 @@ type Props = {
 const WhyChooseCard = ({image, title}: Props) => {
   return (
     <div>
+        <img src={image} alt="image" className='mx-auto' />
+        <h1 className='mt-6 text-center text-gray-900 font-medium text-lg'>{title}</h1>
+        <p className='mt-2 text-center text-xs font-medium text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dignissimos</p>
     </div>
   )
 }
