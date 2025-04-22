267f080b38e13e641be06c1cb7726d9c1cc4f40c
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-22 23:46:35 +0200

    create base structure for News component with section heading

---
+++ components/Home/News/News.tsx
@@ -0,0 +1,12 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
+import React from 'react';
+
+const News = () => {
+    return (
+        <div className='pt-16 pb-16'>
+            <SectionHeading heading='Exciting Travel News for You' />
+        </div>
+    )
+}
+
+export default News;
