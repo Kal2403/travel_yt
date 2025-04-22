7cdda1a6fdae57680f4b28ff4fe2987f69689090
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-22 23:54:27 +0200

    Add responsive grid layout for news cards

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -5,6 +5,9 @@ const News = () => {
     return (
         <div className='pt-16 pb-16'>
             <SectionHeading heading='Exciting Travel News for You' />
+            <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center mt-20'>
+
+            </div>
         </div>
     )
 }
