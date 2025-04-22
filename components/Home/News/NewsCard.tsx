11c56c909e91eaa540b7af1ea99a769d642ca0e2
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-22 23:56:10 +0200

    Create base NewsCard component with props

---
+++ components/Home/News/NewsCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {
+    image: string;
+    date: string;
+    title: string;
+}
+
+const NewsCard = ({date, image, title}: Props) => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default NewsCard
