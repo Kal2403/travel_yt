a11e29f39ed34360515386cd021b5aaf6b334490
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-23 23:52:29 +0200

    Create base Newsletter component structure

---
+++ components/Newsletter/Newsletter.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Newsletter = () => {
+  return (
+    <div className='bg-black pt-16 pb-16 flex items-center justify-center w-full flex-col'>
+    </div>
+  )
+}
+
+export default Newsletter;
