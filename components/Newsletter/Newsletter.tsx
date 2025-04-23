9cba86fbe77bf1e33e7149bbdb225e21cb83c49b
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-23 23:53:30 +0200

    Add icon and heading to Newsletter

--- components/Newsletter/Newsletter.tsx
+++ components/Newsletter/Newsletter.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { BsEnvelopePaper } from 'react-icons/bs';
 
 const Newsletter = () => {
   return (
     <div className='bg-black pt-16 pb-16 flex items-center justify-center w-full flex-col'>
+        <BsEnvelopePaper className='w-16 h-16 mt-20 text-white' />
+        <h1 className='text-lg sm:text-xl md:text-2xl text-white font-semibold mt-10 tracking-widest'>Your Travel Journet Start Here</h1>
     </div>
   )
 }
