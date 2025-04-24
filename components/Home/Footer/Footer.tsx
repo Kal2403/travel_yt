a5e7bde4922a58f35685609f6c52e9f5970de408
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-24 23:35:50 +0200

    Create base Footer component structure

---
+++ components/Home/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='pt-16 pb-16'>
+        <div className='w-[80%] mx-auto items-start grid-cols-1 sm:grid-cols-2 grid md:grid-cols-2 lg:grid-cols-4 gap-10'>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
