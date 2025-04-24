c8443674201c8b783b9f07389062871c9997a741
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-24 23:45:30 +0200

    Add Company, Support, and Other Services columns

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -4,6 +4,30 @@ const Footer = () => {
   return (
     <div className='pt-16 pb-16'>
         <div className='w-[80%] mx-auto items-start grid-cols-1 sm:grid-cols-2 grid md:grid-cols-2 lg:grid-cols-4 gap-10'>
+            <div className='space-y-5'>
+                <h1 className='text-lg font-bold'>Company</h1>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>About Us</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Careers</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Blog</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Gift Cards</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Magazine</p>
+            </div>
+            <div className='space-y-5'>
+                <h1 className='text-lg font-bold'>Support</h1>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Contact</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Legal Notice</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Privacy Policy</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Terms and Conditions</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Sitemap</p>
+            </div>
+            <div className='space-y-5'>
+                <h1 className='text-lg font-bold'>Other Services</h1>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Car Hire</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Activity Finder</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Tour List</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Flight Finder</p>
+                <p className='text-gray-800 font-medium cursor-pointer text-sm hover:text-blue-950'>Travel Agents</p>
+            </div>
         </div>
     </div>
   )
