3c0b30af063628733ce4d7961bc3951de6ec9277
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-25 22:03:18 +0200

    Render button conditionally with styles

--- components/Helper/ScrollToTop.tsx
+++ components/Helper/ScrollToTop.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
+import { FaArrowUp } from 'react-icons/fa';
 
 const ScrollToTop = () => {
 
@@ -26,7 +27,11 @@ const ScrollToTop = () => {
 
     return (
         <div className='fixed bottom-4 animate-pulse right-4'>
-
+            {
+                isVisible && ( <button onClick={scrollToTop} className='bg-rose-700 text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none'>
+                    <FaArrowUp />
+                </button>
+            )}
         </div>
     )
 }
