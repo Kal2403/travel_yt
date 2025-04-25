7d3551fcdf395646da2ace74f7cab9a51d80b079
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-25 21:57:04 +0200

    Set up base ScrollToTop component and Add to layout

---
+++ components/Helper/ScrollToTop.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+
+const ScrollToTop = () => {
+
+    return (
+        <div className='fixed bottom-4 animate-pulse right-4'>
+
+        </div>
+    )
+}
+
+export default ScrollToTop;
