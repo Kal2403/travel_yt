379c61b487cbfb6b02e8dc10d77911bedefdcedd
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-25 22:00:19 +0200

    Add scroll visibility logic with useEffect

--- components/Helper/ScrollToTop.tsx
+++ components/Helper/ScrollToTop.tsx
@@ -1,7 +1,21 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ScrollToTop = () => {
+
+    const [isVisible, setVisible] = useState(false);
+
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 300) {
+                setVisible(true);
+            } else {
+                setVisible(false);
+            }
+        };
+        window.addEventListener("scroll", toggleVisibility);
+        return () => window.removeEventListener("scroll", toggleVisibility);
+    }, []);
 
     return (
         <div className='fixed bottom-4 animate-pulse right-4'>
